7e7a476bc8ae57663a60947ac674e1c3d99a6df8
Angel Alexis Mora Silva <al17760247@ite.edu.mx>
2022-10-12 14:57:37 -0700

    authentification

---
+++ pages/authentification/initFirebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebase from 'firebase'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCznrnvmGsKnbSbSmoQP1KNj2fNUuTxEQQ",
+  authDomain: "smaedbxenia9no.firebaseapp.com",
+  databaseURL: "https://smaedbxenia9no-default-rtdb.firebaseio.com",
+  projectId: "smaedbxenia9no",
+  storageBucket: "smaedbxenia9no.appspot.com",
+  messagingSenderId: "410302656960",
+  appId: "1:410302656960:web:93a7600510ca2f72955825",
+  measurementId: "G-XPRTC5VFDY"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); 
+
+export default app;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,58 @@
 import React from 'react'
-import Cabecera from '../pages/cabecera'
-import { TextInput, Group, Button, Stack, AppShell,Navbar, Header, Anchor } from '@mantine/core';
+import { TextInput, Group, Button, Stack, Box , Container, Center} from '@mantine/core';
+import { useState } from 'react';
+import app from './authentification/initFirebase';
+import firebase from 'firebase'
 
 const Home = () => {
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = (e) => {
+    e.preventDefault();
+    let usuario = user;
+    let contraseña = password;
+
+    app.auth().signInWithEmailAndPassword(usuario, contraseña).then(u => {})
+    .catch(function(error)
+    {console.log(error)}
+    );
+  } 
+
+  const signUp = (e) => {
+    let usuario = user;
+    let contraseña = password;
+    app.auth().createUserWithEmailAndPassword(usuario, contraseña).then(u = {})
+    .catch(function(error)
+    {console.log(error)}
+    );
+  }
+
   return (
-      <Cabecera>
-        <Stack position='center' spacing='xs'>
+    <div>
+      <Container size={200} px={0}>
           <TextInput
-          placeholder="Your name"
+          placeholder="Your username"
           description = "Texto de descripcion"
+          value={user}
+          onChange = { (e) => setUser(e.currentTarget.value)}
           withAsterisk
           />
           <TextInput
-          placeholder="Your phone number"
-          style={{
-            textEmphasisColor : "blue"
-          }}
+          placeholder="Your password"
           description = "Texto de descripcion 2"
+          value={password}
+          onChange= {(e) => setPassword(e.currentTarget.value)}
           withAsterisk = "false"
           />
-        </Stack>
-        {/* <Group position="apart" spacing="xl" grow>
-      <Button color="cyan" variant="filled">1</Button>
-      <Button variant="outline">2</Button>
-      <Button variant="outline">3</Button>
-    </Group> 
-    */}
-      </Cabecera>
+          <Center>
+      <Button >Ingresar</Button>
+          </Center>
+    </Container>
+    </div>
+       
+      
       
   )
 }
